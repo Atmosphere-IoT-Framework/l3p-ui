77e382cac942432c1143006b79e9f0429981554b
Riccardo Berta <riccardo.berta@gmail.com>
2020-01-21 16:54:19 +0100

    -

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   configureWebpack :{
     plugins: [new HtmlWebpackPlugin({
         template: './src/index.html'
-    })],
+    })], 
     externals: {
         // global app config object
         config: JSON.stringify({
