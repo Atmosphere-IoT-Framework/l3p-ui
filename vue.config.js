14c60ee45839578fff223f1db87221a348248dcd
Riccardo Berta <riccardo.berta@gmail.com>
2020-01-26 19:30:49 +0100

    -

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:8084/v1' 
+            apiUrl: 'http://localhost:8084/v1'  
         })
     } 
   }
